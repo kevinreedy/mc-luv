eb99644a2da2d71b584fee7db1db97f9b6adc42e
Robb Kidd <robb@thekidds.org>
2017-03-27 22:40:17 -0400

    wrapping stuff
    Flight class to wrap flyday's flights.
    
    A little Segment class to contain the logic of displaying segments.

---
+++ lib/segment.rb
@@ -0,0 +1,14 @@
+class Segment
+  attr_accessor :flight_number, :stop_no_plane_change, :has_wifi
+  def initialize(flyday_segment)
+    @flight_number = flyday_segment['operatingCarrierInfo']['flightNumber']
+    @stop_no_plane_change = flyday_segment['numberOfStops'] > 0
+    @has_wifi = flyday_segment['wifiAvailable']
+  end
+
+  def to_s
+    "#{flight_number} " +
+    (stop_no_plane_change ? ' <i class="fa fa-map-signs" style="color:#ff8c00"></i>' : '') +
+    (has_wifi ? ' <i class="fa fa-wifi text-success"></i>' : ' <i class="fa fa-wifi text-danger"></i>')
+  end
+end
